3512b2c80d76ee82cff68f2bedad6aa66bf15202
AnoopAG91 <anoopag47@gmail.com>
2023-07-20 19:18:40 +0530

    made changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,32 +3,32 @@ import './Navbar.css'
 //FONT AWESOME ICON IMPORTS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars} from '@fortawesome/free-solid-svg-icons'
-import {useNavigate} from 'react-router-dom'
-
+import {useNavigate,} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function Navbar() {
     const navigate=useNavigate()
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className='container'>
 
-            <a class="navbar-brand"><button onClick={()=>navigate("/")}><span>A</span>NOOP</button></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <Link className="navbar-brand"><button onClick={()=>navigate("/")}><span>A</span>NOOP</button></Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <FontAwesomeIcon icon={faBars} style={{color:"#fff"}} />
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
-                    <a className="nav-link"><button onClick={()=>navigate("/")}>Home</button></a>
+                    <Link className="nav-link"><button onClick={()=>navigate("/")}>Home</button></Link>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" ><button onClick={()=>navigate("/About")}>About</button></a>
+                    <Link className="nav-link" ><button onClick={()=>navigate("/About")}>About</button></Link>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" ><button onClick={()=>navigate("/Projects")}>Projects</button></a>
+                    <Link className="nav-link" ><button onClick={()=>navigate("/Projects")}>Projects</button></Link>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link"><button onClick={()=>navigate("/Contact")}>Contact</button></a>
+                    <Link className="nav-link"><button onClick={()=>navigate("/Contact")}>Contact</button></Link>
                 </li>
                 </ul>
             </div>
